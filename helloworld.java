a9a7cdc56953ab4c0aa3a769d43d0f7c62ec514d
sushreesinhar <99821920+sushreesinhar@users.noreply.github.com>
2022-03-06 16:53:31 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World : Triggered by Webhook!!!!");
+    System.out.println("Hello World : Triggered by Github!!!!");
     }
 }
